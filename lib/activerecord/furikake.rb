ab3be397246df78595dcc625b802db21f10233c0
litswd <hiromu.kariya@gmail.com>
2023-04-17 23:34:45 +0900

    Initial commit

---
+++ lib/activerecord/furikake.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "furikake/version"
+
+module Activerecord
+  module Furikake
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
